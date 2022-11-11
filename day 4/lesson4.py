633cc45d97e33252e87047205f98fccc7db3c2e8
NikoNeparidze <nikoneparidze7@gmail.com>
2022-11-11 14:10:59 +0400

    fourth commit

---
+++ day 4/lesson4.py
@@ -0,0 +1,74 @@
+full_name=("niko neparidze")
+
+# print(full_name)
+
+# print(full_name[:5])
+# print(full_name[5:])
+# print(full_name[:8])
+# print(full_name[8:])
+
+# print(full_name[-1])
+# print(full_name[-5])
+
+#print(full_name[-1:-4:-1])
+
+# print(full_name.upper())
+
+#print(full_name.lower())
+# name2="    niko    "
+#print(name2)
+#print(name2.strip())
+
+#print(full_name.replace(" ", "#"))
+# print(full_name.replace
+# sentance= ("nika keshelava is good mentor and its")
+
+# proggram_knows=(True) 
+# print(sentance +" " +str(proggram_knows))
+
+
+alphabet=("abcdefghijklmnopqrstuvwxyz")
+
+letter1=(alphabet.upper()[13])
+letter2=(" "+alphabet[8])
+letter3=("  "+alphabet[10])
+letter4=("   "+alphabet[14])
+print(letter1)
+print(letter2)
+print(letter3)
+print(letter4)
+letter5=("     "+alphabet.upper()[13])
+letter6=("      "+alphabet[4])
+letter7=("       "+alphabet[-11])
+letter8=("        "+alphabet[0])
+letter9=("         "+alphabet[-9])
+letter10=("          "+alphabet[8])
+letter11=("           "+alphabet[3])
+letter12=("            "+alphabet[-1])
+letter13=("             "+alphabet[4])
+print(" ")
+print(letter5)
+print(letter6)
+print(letter7)
+print(letter8)
+print(letter9)
+print(letter10)
+print(letter11)
+print(letter12)
+print(letter13)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
