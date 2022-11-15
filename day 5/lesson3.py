dc6875bea9f837234a08d1e401a8f54fffd09c97
NikoNeparidze <nikoneparidze7@gmail.com>
2022-11-15 17:55:26 +0400

    Fifth Commit

---
+++ day 5/lesson3.py
@@ -0,0 +1,118 @@
+# my_sentane = "My name {0} My age {2} My sunrame {1}".format("niko", "neparidze", "18")
+
+# print(my_sentane)
+
+
+# my_name = "niko"
+
+# if "a" in my_name:
+#     print("sheicavs i-s")
+# else:
+#     print("ar sheicavs i-s")
+
+
+
+#input              ინფორმაცია შედის 
+#output             ინფორმაცია გამოდის
+
+# my_name = input("enter you name: ")
+
+# print("chemi saxelia "+ my_name)
+
+# my_age = 18
+# my_age +=3
+# print(my_age)
+
+# print("Hello Please Register yourself for future:")
+# print()
+# my_name = input("Enter Your Name: ")
+# my_surname = input("Enter Your Surname: ")
+# print()
+
+# print("welcome: "+ my_name+" " + my_surname)
+# print()
+# my_age = input("You Forgot To Enter Your age: ")
+# print()
+# print("Congratulation, you submited your info come back in when you want. For now you info is:")
+# print()
+# print("My name is {}. My surname is {}. My age is {}.".format(my_name, my_surname, my_age))
+# print("<Many Year Later>")
+# year = input("Enter How Long It Took You To Come back: ")
+# print()
+# new_age = int(my_age) + int(year)
+# print("After {} Years I Am Now {} Years Old".format(year,new_age))
+# print()
+# print("End of you Journey, have nice life <3")
+
+
+
+# print("-----Hello, Let's become lucky today-----")
+# print()
+# number1 = input("Enter Your First Number: ")
+# print()
+# number2 = input("Enter Your Second Number: ")
+# print()
+# print("Your Numbers Will Be Multiplied If The Answer Will Be Hingher Than A 100 YOU LOSE. ")
+# sum = int(number1) * int(number2)
+# lose = ("Unfortunately You Lose, Your Sum Number Is Higher Than 100.")
+# win = ("You win, your answer is beloew 100. your winning number is: ") + str(sum)
+# print()
+# print ("Your Sum Is: " + str(sum))
+# print (win) if sum < int("100") else print(lose)
+
+
+
+# odd = (1 , 3 , 5 , 7 , 9)
+# even = (2 , 4 , 6 , 8 , 0)
+
+# num1 = int(input("Enter Number One : "))
+# num2 = int(input("Enter Number Two : "))
+# num3 = int(input("Enter Number Three : "))
+
+# sum = num1 + num2 + num3 
+
+#  evensum = ("The Answer Is Not Acceptable For System.")
+#  oddsum = ("The Answer Is Accaptable For System Congggggggggggggggggggrats.")
+# print ("Your Sum is : ")
+
+# if sum==(1,3,5,7,9) : print(sum) 
+# if  sum==(2,4,6,8,0,) :print("you lose")
+#  if sum is odd :
+#      print("you lose")
+
+name1 = (input("Enter Name 1 :"))
+name2 = (input("Enter Name 2 : "))
+
+
+
+
+
+print ("a" , "e" , "i" , "o", "u") in name1 , name2
+if True :
+        print( name1 or name2 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
